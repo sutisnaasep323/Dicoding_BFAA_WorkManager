0dab94a6e10fa891f416beca4c2b60f32e489cb4
Asep Sutisna <sutisnaasep323@gmail.com>
2021-10-02 08:28:41 +0700

    Menampilkan data cuaca & mengatur task

--- app/src/main/java/com/example/myworkmanager/MainActivity.kt
+++ app/src/main/java/com/example/myworkmanager/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.myworkmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.work.*
+import com.example.myworkmanager.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var workManager: WorkManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        workManager = WorkManager.getInstance(this)
+        binding.btnOneTimeTask.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btnOneTimeTask -> StartOneTimeTask()
+        }
+    }
+
+    private fun StartOneTimeTask() {
+        binding.textStatus.text = (getString(R.string.status))
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+
+        // mengatur task ketika koneksi internet terhubung
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .setConstraints(constraints)
+            .build()
+
+        workManager.enqueue(oneTimeWorkRequest)
+        workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
+            .observe(this@MainActivity, { workInfo ->
+                val status = workInfo.state.name
+                binding.textStatus.append("\n" + status)
+            })
+    }
+
+
 }
